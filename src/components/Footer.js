0d26330971abccd9b2ee5b5d99d7e78a89d5f059
monukmodi <monukr2012@gmail.com>
2024-01-03 11:58:56 +0530

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react"
 
 const Footer = () => {
   return (
-    <footer className='bg-black py-8 text-center text-white'>
-      <div className='container mx-auto'>
-        Copyright &copy; 2022. All rights reserved.
+    <footer className="bg-black py-8 text-center text-white">
+      <div className="container mx-auto">
+        Copyright &copy; 2023. All rights reserved.
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
